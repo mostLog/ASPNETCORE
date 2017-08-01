4904bbf16cf2a8f70fe425f2a656879d022c779a
小白 <Administrator@>
2017-08-01 17:24:29 +0800

    0.0.2

---
+++ L.SpiderCore/Event/OnCompleteEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+
+
+namespace L.SpiderCore.Event
+{
+    /// <summary>
+    /// 当爬虫执行完参数传递
+    /// </summary>
+    public class OnCompleteEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 爬取地址
+        /// </summary>
+        public string Url { get; set; }
+        public OnCompleteEventArgs()
+        {
+            
+        }
+    }
+}
---
+++ L.SpiderCore/Tools/Selector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace L.SpiderCore.Tools
+{
+    /// <summary>
+    /// 解析html工具类
+    /// </summary>
+    public class Selector
+    {
+        public Selector()
+        {
+
+        }
+    }
+}
