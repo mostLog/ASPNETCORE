a162799733cdd2ef2a214a3438a2ba68f6b4800a
小白 <Administrator@>
2017-09-19 17:45:26 +0800

    禁用右键

--- L.Web/wwwroot/js/business/home/home.js
+++ L.Web/wwwroot/js/business/home/home.js
@@ -14,6 +14,30 @@
             redict();
         }
     }
+    
+    function nocontextmenu() {
+        event.cancelBubble = true
+        event.returnValue = false;
+
+        return false;
+    }
+
+    function norightclick(e) {
+        if (window.Event) {
+            if (e.which == 2 || e.which == 3)
+                return false;
+        }
+        else {
+            if (event.button == 2 || event.button == 3) {
+                event.cancelBubble = true
+                event.returnValue = false;
+                return false;
+            }
+        }
+            
+    }
+    document.oncontextmenu = nocontextmenu; 
+    document.onmousedown = norightclick;
 
     function redict() {
         window.location.href = "https://www.baidu.com/";
